5fad221a58a0b754747b59568584077d5725a967
Ale007XD <alex.deloverov@gmail.com>
2025-08-03 16:27:51 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/GittiNew/' // для GitHub Pages! Имя репозитория
+  base: '/GittiNew/', // Это важно для GitHub Pages
 })
